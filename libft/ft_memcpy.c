944affe4d6e422dde85034ce6f841be70ba14838
fwhite42 <fwhite42@protonmail.com>
2024-01-01 21:34:06 +0100

    cheating on memcpy ?

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,23 +6,13 @@
 /*   By: fwhite42 <FUCK THE NORM>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 09:36:42 by fwhite42          #+#    #+#             */
-/*   Updated: 2024/01/01 21:09:50 by fwhite42         ###   ########.fr       */
+/*   Updated: 2024/01/01 21:33:38 by fwhite42         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdlib.h>
+#include"libft.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char	*d;
-	char	*s;
-
-	if (dst != src)
-	{
-		d = (char *)dst;
-		s = (char *)src;
-		while (n--)
-			*(d++) = *(s++);
-	}
-	return (dst);
+	return (ft_memmove(dst, src, n));
 }
